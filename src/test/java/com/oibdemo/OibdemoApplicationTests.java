a0fe0fed9e685683d836061a331b5c8875929195
Sachin Dattatray Mengade (External - Workforce Logiq Sweden AB (SE)) <sachindattatray.mengade@ingka.com>
2022-03-30 13:26:27 +0200

    HRTR-2839:Added demo project to github.com

---
+++ src/test/java/com/oibdemo/OibdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oibdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OibdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
