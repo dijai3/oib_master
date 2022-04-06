2948af70150e7486be20acda6c4c0e9473f82aa3
SachinM26 <102593963+SachinM26@users.noreply.github.com>
2022-04-06 10:13:13 +0200

    HRTR-2839:test

--- src/main/java/com/ikea/oibmb/controller/BusinessLogicController.java
+++ src/main/java/com/ikea/oibmb/controller/BusinessLogicController.java
@@ -3,6 +3,7 @@ import java.util.List;
 
 import com.ikea.oibmb.pojo.Payroll;
 import com.ikea.oibmb.service.BusinessLogicService;
+import com.ikea.oibmb.serviceImpl.BusinessLogicServiceImpl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class BusinessLogicController {
 
     @Autowired
-    private BusinessLogicService service;
+    private BusinessLogicServiceImpl service;
 
     @GetMapping("/hello")
     public  List<Payroll> helloWorld(){
